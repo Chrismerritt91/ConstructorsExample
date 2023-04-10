bed223fdfb7f3a4cd3f2c921879b0b4e3f71ece5
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-10 15:50:21 -0500

    complete first lesson of constructors lessons

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
 
+static void Main() {
+
+        //create three objects for employees
+        Employee emp1 = new Employee(101, "Scott", "Manager");
+        Employee emp2 = new Employee(102, "Allen", "Asst. Manager");
+        Employee emp3 = new Employee(103, "Anna", "Clerk");
+
+        //display fields
+        Console.WriteLine("First Employee:");
+        Console.WriteLine(emp1.empID);
+        Console.WriteLine(emp1.empName);
+        Console.WriteLine(emp1.job);
+        Console.WriteLine();
+
+        Console.WriteLine("Second Employee:");
+        Console.WriteLine(emp2.empID);
+        Console.WriteLine(emp2.empName);
+        Console.WriteLine(emp2.job);
+        Console.WriteLine();
+
+        Console.WriteLine("Third Employee:");
+        Console.WriteLine(emp3.empID);
+        Console.WriteLine(emp3.empName);
+        Console.WriteLine(emp3.job);
+        Console.WriteLine();
+
+        Console.ReadKey();
+
+
+    }
+
+
+}
