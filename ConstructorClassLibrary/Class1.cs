b6ffe42c8130b55a1a061a21de774c56c85f3a2e
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-12 15:51:44 -0500

    complete lessons on properties

---
+++ ConstructorClassLibrary/Class1.cs
@@ -0,0 +1,138 @@
+﻿public class Employee
+{
+
+    //fields
+    private int _empID;
+    private string _empName;
+    private string _job;
+    private double _salary;
+    private double _tax;
+
+    //instance properties
+    public int EmpID
+    {
+        set
+        {
+            if (value >= 100)
+            {
+                this._empID = value;
+            }
+        }
+        get { return _empID; }
+    }
+    public string EmpName { get { return _empName; } set { this._empName = value; } }
+    public string Job { get { return _job; } set { this._job = value; } }
+
+    //static field
+    private static string _companyName;
+
+    //static property
+    public static string CompanyName
+    {
+        get { return _companyName; }
+        set
+        {
+            if (value.Length <= 20)
+            {
+                _companyName = value;
+            }
+        }
+    }
+
+    //Explicit constructors
+    //Instance Constructors
+
+    public Employee(int empID, string empName, string job)
+    {
+        this._empID = empID;
+        this._empName = empName;
+        this._job = job;
+        this._salary = 1000;
+    }
+
+    public Employee(int empID, string empName)
+    {
+        this._empID = empID;
+        this._empName = empName;
+        this._salary = 1000;
+    }
+
+    public Employee()
+    {
+        _empID = 1;
+        this._salary = 1000;
+    }
+
+
+    //static constructor
+    static Employee()
+    {
+        _companyName = "ABC Industries";
+    }
+
+    //readonly property
+    public double Salary { get { return _salary; } }
+
+    //writeonly property
+    public double Tax
+    {
+        set
+        {
+            if (value >= 0 && value <= 100)
+            {
+                this._tax = value;
+            }
+        }
+    }
+
+    //method using readonly and writeonly
+    public double CalculateNetSalary()
+    {
+        double t = Salary - _tax;
+        return t;
+    }
+
+    //automatic property
+    public string NativePlace { get; set; } = "New York";
+
+
+
+
+
+    //Getters and Setters are no longer needed when properties are being used, simpler syntax
+    ////Getters
+
+    //public int getEmpID()
+    //{
+    //    return this.empID;
+    //}
+
+    //public string getEmpName()
+    //{
+    //    return this.empName;
+    //}
+
+    //public string getJob()
+    //{
+    //    return this.job;
+    //}
+
+    ////Setters
+
+    //public void setEmpID(int value)
+    //{
+    //    this.empID = value;
+    //}
+
+    //public void setEmpName(string value)
+    //{
+    //    this.empName = value;
+    //}
+
+    //public void setJob(string value)
+    //{
+    //    this.job = value;
+    //}
+
+
+}
--- Program.cs
+++ Program.cs
@@ -4,33 +4,42 @@ static void Main() {
 
         //create three objects for employees
         Employee emp1 = new Employee();//parameter-less constructor
-        emp1.setEmpID(101);
-        emp1.setEmpName("Scott");
-        emp1.setJob("Manager");
+        emp1.EmpID = 101;
+        emp1.EmpName = "Scott";
+        emp1.Job = "Manager";
+        emp1.Tax = 50;
+        emp1.NativePlace = "New Dehli";
         Employee emp2 = new Employee(102, "Allen");
-        emp2.setJob("Asst. Manager");
+        emp2.Job = "Asst. Manager";
         Employee emp3 = new Employee(103, "Anna", "Clerk");
-        //setting fields using object initializer
-        Employee emp4 = new Employee(){empName = "Ford",job = "Executive"};
+        //setting fields using object initializer/ use property names instead of field name
+        Employee emp4 = new Employee(){EmpName = "Ford",Job = "Executive"};
 
         //display fields
-        Console.WriteLine(Employee.companyName);
+        Console.WriteLine(Employee.CompanyName);
         Console.WriteLine("\nFirst Employee:");
-        Console.WriteLine(emp1.getEmpID());
-        Console.WriteLine(emp1.getEmpName());
-        Console.WriteLine(emp1.getJob());
+        Console.WriteLine(emp1.EmpID);
+        Console.WriteLine(emp1.EmpName);
+        Console.WriteLine(emp1.Job);
+        Console.WriteLine(emp1.Salary);
+        Console.WriteLine(emp1.CalculateNetSalary());
+        Console.WriteLine(emp1.NativePlace);
         Console.WriteLine();
 
         Console.WriteLine("Second Employee:");
-        Console.WriteLine(emp2.getEmpID());
-        Console.WriteLine(emp2.getEmpName());
-        Console.WriteLine(emp2.getJob());
+        Console.WriteLine(emp2.EmpID);
+        Console.WriteLine(emp2.EmpName);
+        Console.WriteLine(emp2.Job);
+        Console.WriteLine(emp2.Salary);
+        Console.WriteLine(emp2.NativePlace);
         Console.WriteLine();
 
         Console.WriteLine("Third Employee:");
-        Console.WriteLine(emp3.getEmpID());
-        Console.WriteLine(emp3.getEmpName());
-        Console.WriteLine(emp3.getJob());
+        Console.WriteLine(emp3.EmpID);
+        Console.WriteLine(emp3.EmpName);
+        Console.WriteLine(emp3.Job);
+        Console.WriteLine(emp3.Salary);
+        Console.WriteLine(emp3.NativePlace);
         Console.WriteLine();
 
         Console.ReadKey();
