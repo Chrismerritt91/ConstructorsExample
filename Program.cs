4c86367d15883fd16c25cdcad87fb62638869903
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-11 20:58:38 -0500

    complete more constructors lessons, having issue showing changes in class library

--- Program.cs
+++ Program.cs
@@ -3,28 +3,32 @@
 static void Main() {
 
         //create three objects for employees
-        Employee emp1 = new Employee(101, "Scott", "Manager");
-        Employee emp2 = new Employee(102, "Allen", "Asst. Manager");
+        Employee emp1 = new Employee();//parameter-less constructor
+        emp1.setEmpID(101);
+        emp1.setEmpName("Scott");
+        emp1.setJob("Manager");
+        Employee emp2 = new Employee(102, "Allen");
+        emp2.setJob("Asst. Manager");
         Employee emp3 = new Employee(103, "Anna", "Clerk");
 
         //display fields
         Console.WriteLine(Employee.companyName);
         Console.WriteLine("\nFirst Employee:");
-        Console.WriteLine(emp1.empID);
-        Console.WriteLine(emp1.empName);
-        Console.WriteLine(emp1.job);
+        Console.WriteLine(emp1.getEmpID());
+        Console.WriteLine(emp1.getEmpName());
+        Console.WriteLine(emp1.getJob());
         Console.WriteLine();
 
         Console.WriteLine("Second Employee:");
-        Console.WriteLine(emp2.empID);
-        Console.WriteLine(emp2.empName);
-        Console.WriteLine(emp2.job);
+        Console.WriteLine(emp2.getEmpID());
+        Console.WriteLine(emp2.getEmpName());
+        Console.WriteLine(emp2.getJob());
         Console.WriteLine();
 
         Console.WriteLine("Third Employee:");
-        Console.WriteLine(emp3.empID);
-        Console.WriteLine(emp3.empName);
-        Console.WriteLine(emp3.job);
+        Console.WriteLine(emp3.getEmpID());
+        Console.WriteLine(emp3.getEmpName());
+        Console.WriteLine(emp3.getJob());
         Console.WriteLine();
 
         Console.ReadKey();
